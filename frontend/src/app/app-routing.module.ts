616374deff628c290d59827a22cac3f18fbfd690
Yojhan <yojhanr95@gmail.com>
2019-09-07 10:42:33 -0500

    Create routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
+import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'accounts/login', 
+    component: LoginComponent
+  },
+  {
+    path: 'accounts/signup', 
+    component: SignupComponent
+  },
+  {
+    path: 'profile', 
+    component: ProfileComponent
+  },
+  {
+    path: 'accounts/password/reset', 
+    component: RequestResetComponent
+  },
+  {
+    path: 'accounts/password/response', 
+    component: ResponseResetComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
